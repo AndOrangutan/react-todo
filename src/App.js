e276052490cac773f48905e66a0d66a229816b69
Von Mueller <vonjonmueller@gmail.com>
2023-02-22 20:08:19 -0800

    Created Hello World

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  // Since we must return everything in a single component, we use a fragment or empty < >
+  return (
+    < >
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
