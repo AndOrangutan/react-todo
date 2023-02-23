d1995263322bf9414beaa62b3701bcfa052de103
Von Mueller <vonjonmueller@gmail.com>
2023-02-22 20:58:23 -0800

    Added styling to the page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
-const Header = () => {
+// const Header = (props) => {
+//   return (
+//     <header>
+//       <h1>{props.title}</h1>
+//     </header>
+//   )
+// }
+
+// Can also destructure props to directly acces title
+const Header = ({title}) => {
   return (
     <header>
-      <h1>Task Tracker</h1>
+      <h1>{title}</h1>
     </header>
   )
 }
+
+
 
 export default Header
